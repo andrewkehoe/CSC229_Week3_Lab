f6cfa713c7f723084d9ccfb50f0e8f866dbe8319
Andrew Kehoe <andrewkehoe0@gmail.com>
2024-02-11 19:20:57 -0500

    Update NewClass.java

--- src/main/java/com/mycompany/week3_lab/NewClass.java
+++ src/main/java/com/mycompany/week3_lab/NewClass.java
@@ -2,17 +2,26 @@
 package com.mycompany.week3_lab;
 
 public class NewClass {
+    /* Computing Big O Notation - Andrew Kehoe CSC229
+        In order to compute the efficiency of this algorithm, we have to determine the amount of steps.
+    */
     int getLargest(int[] nmList ){
-        int max= nmList [0];
-        int indx = 0;
-        while( indx< nmList.length ) {
-            if (nmList [indx] > max){
-                    max= nmList [indx];
+        int max= nmList [0]; //Step 1 is declaring this integer.
+        int indx = 0;        //Step 2 is declaring another integer.
+        
+        //At this point, we begin a while loop...
+        //It goes for as long as nmList.length is, which is "n".
+        while( indx< nmList.length ) { //Every while test runs "n" times.
+            if (nmList [indx] > max){  //Every if test also runs "n" times.
+                    max= nmList [indx];//Every definition of max also runs a maximum of "n" times, worst case scenario.
               }
-            indx++;
+            indx++; //Every addition runs "n" times.
         }
-        return max;
-    }
+        return max; //Finally, the algorithm returns 1 time.
+    } //Therefore, the amount of steps is 3 constant, and 4n, for as big as data "n" is. It is LINEAR.
+      //In other words, there are 3+4n steps.
+    
+    
     public static void main(String[] args) {
         NewClass nc = new NewClass();
         int[] arr = { 3, 6, -1, 10, 15,-8};
